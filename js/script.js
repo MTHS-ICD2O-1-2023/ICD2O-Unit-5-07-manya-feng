d3a31cb34d6a91c5e0dc43d39f85b5edbef9d87b
Manya Feng <159185413+manya-feng@users.noreply.github.com>
2024-05-10 03:39:39 +0000

    change

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Manya All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Manya
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function does multiplication
+ */
+function myButtonClicked() {
+  var numA = parseFloat(document.getElementById("inputNumber").value)
+  var product = 0
+  var counter = 0
+
+  while (counter <= numA) {
+    product = product + counter
+    counter++
+  }
+
+  document.getElementById("product").innerHTML = "The product is: " + product
+}
